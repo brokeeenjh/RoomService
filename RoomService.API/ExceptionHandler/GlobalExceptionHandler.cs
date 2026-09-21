79fe6d544ecf8cfdf23c5069c24b303117b209f8
brokeeenjh <mouzer611@gmail.com>
2026-09-21 23:15:02 +0300

    exceptionHandler added

---
+++ RoomService.API/ExceptionHandler/GlobalExceptionHandler.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace RoomService.API.ExceptionHandler;
+
+public class GlobalExceptionHandler
+{
+    public RequestDelegate _next;
+    public ILogger<GlobalExceptionHandler> _logger;
+    
+    public GlobalExceptionHandler(RequestDelegate next, ILogger<GlobalExceptionHandler> logger)
+    {
+        _next = next;
+        _logger = logger;
+    }
+
+    public async Task InvokeAsync(HttpContext context)
+    {
+        try
+        {
+        await _next(context);
+        }
+        catch (Exception e)
+        {
+            _logger.LogError(e.Message);
+
+            context.Response.StatusCode = e switch
+            {
+                ApplicationException => StatusCodes.Status400BadRequest,
+                _ => StatusCodes.Status500InternalServerError,
+            };
+
+            await context.Response.WriteAsJsonAsync(new ProblemDetails
+            {
+                Type = e.GetType().Name,
+                Title = "An error occured",
+                Detail = e.Message,
+            });
+        }
+    }
+}
--- RoomService.Application/obj/Debug/net10.0/RoomService.Application.AssemblyInfo.cs
+++ RoomService.Application/obj/Debug/net10.0/RoomService.Application.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("RoomService.Application")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f3553286cad5e47a0ed687ed4583d03d69f1146a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ea04d1e95d145ec3aa5e435cc5d25a19f292afd7")]
 [assembly: System.Reflection.AssemblyProductAttribute("RoomService.Application")]
 [assembly: System.Reflection.AssemblyTitleAttribute("RoomService.Application")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
